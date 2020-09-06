2380977fa073c1c1b377c26bf448afd36d6bc103
Avik Sengupta <avik@sengupta.net>
2020-09-06 18:29:26 +0100

    reversediff -> reverse_diff

--- src/precompile_mwe.jl
+++ src/precompile_mwe.jl
@@ -1,6 +1,6 @@
 module precompile_mwe
     using Turing, ReverseDiff, Memoization
-    Turing.setadbackend(:reversediff)
+    Turing.setadbackend(:reverse_diff)
     Turing.setrdcache(true)
     greet() = print("Hello World!")
 end # module
